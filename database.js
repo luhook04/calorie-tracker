8c883265f2e212948b2bfe4494a2e8cc41fee43a
Luke Hook <lukehook04@gmail.com>
2023-05-13 18:59:31 -0400

    Switch to module for importing dotenv

--- database.js
+++ database.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
+import * as dotenv from 'dotenv';
+dotenv.config();
 import mysql from 'mysql2';
 
-const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: process.env.DB_PASSWORD,
-  database: 'calorie_tracker',
-});
+const pool = mysql
+  .createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'calorie_tracker',
+  })
+  .promise();
