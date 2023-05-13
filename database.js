b985114ed9e3d5d28ef8b13ee3420f260b485281
Luke Hook <lukehook04@gmail.com>
2023-05-13 18:56:17 -0400

    Use .env to connect mysql database

---
+++ database.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+  host: '127.0.0.1',
+  user: 'root',
+  password: process.env.DB_PASSWORD,
+  database: 'calorie_tracker',
+});
