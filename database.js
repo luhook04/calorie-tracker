37386c6be4d6d1f9b3baa1fc02b99e73dd8cf2e6
Luke Hook <lukehook04@gmail.com>
2023-05-15 17:37:00 -0400

    Use environment variables for pool creation

--- database.js
+++ database.js
@@ -4,9 +4,16 @@ import mysql from 'mysql2';
 
 const pool = mysql
   .createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: process.env.DB_PASSWORD,
-    database: 'calorie_tracker',
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB,
   })
   .promise();
+
+const getUsers = async () => {
+  const [result] = await pool.query('SELECT * FROM user');
+  return result;
+};
+
+console.log(result);
